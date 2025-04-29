518ce24c2548cdd8b19d1c98c3a6ee103f0e23b6
Samreen Rafiq <samreen@aptech.com>
2025-04-29 16:50:19 +0500

    Validations and unique Email

--- backhand/controller/user_logic.js
+++ backhand/controller/user_logic.js
@@ -1,13 +1,23 @@
 let user = require("../collection/User")
+let bcrypt = require("bcrypt");
+
  let user_function={
     register: async function(req,res){
         try{
-            let user_data = new user(req.body);
+            let {name,email,password,gender,age} = req.body;
+            let emailcheck = user.findOne({email:email});
+            if (emailcheck) {
+                return res.status(409).json({msg:"Email Already Exist"})
+            } else {
+                let haspassword = bcrypt.hashSync(password,13)
+            let user_data = new user({name,email,password:haspassword,gender,age});
             let save_data = await user_data.save();
             res.status(200).json({msg: "User registered successfully"});
+            }
             }catch(error){
                 res.status(501).json({msg:error.message});
         }
     }
+    
  }
  module.exports=user_function;
